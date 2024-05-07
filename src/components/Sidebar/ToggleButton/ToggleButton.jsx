7bdfe004cb310afb90af0fdc11ea4a0ea168d3ce
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-07 12:43:57 -0400

    Add ToggleButton

--- src/components/Sidebar/ToggleButton/ToggleButton.jsx
+++ src/components/Sidebar/ToggleButton/ToggleButton.jsx
@@ -1,6 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+
 // eslint-disable-next-line react/prop-types
-const ToggleButton = ({ setOpen }) => {
-  return <button onClick={() => setOpen((prev) => !prev)}>vt</button>;
+const ToggleButton = ({ setOpen, open }) => {
+  return (
+    <button onClick={() => setOpen((prev) => !prev)}>
+      {open ? (
+        <FontAwesomeIcon icon={faX} />
+      ) : (
+        <FontAwesomeIcon icon={faBars} />
+      )}
+    </button>
+  );
 };
 
 export default ToggleButton;
