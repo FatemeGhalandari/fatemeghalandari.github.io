cdd9629d0f25cb0ac4ee59fdabafbf07a51a24db
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-09 14:17:32 -0400

    Add parallax components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import "./App.scss";
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Parallax from "./components/Parallax/Parallax";
 function App() {
   return (
-    <>
+    <div>
       <section id="Home">
         <Navbar />
+        <Hero />
       </section>
-      <section id="Services">parallax</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>services</section>
-      <section id="Portfolio">parallax</section>
+      <section id="Skills">
+        <Parallax type="skills" />
+      </section>
       <section>portfolio1</section>
       <section>portfolio2</section>
       <section>portfolio3</section>
       <section id="Contact">contact</section>
-    </>
+    </div>
   );
 }
 
