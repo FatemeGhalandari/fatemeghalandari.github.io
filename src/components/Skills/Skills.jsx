46f5cc57514efa828ffde29268b9adb84e4bbed3
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-10 10:26:08 -0400

    Add skills component

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,73 @@
+import "./Skills.scss";
+
+const Skills = () => {
+  return (
+    <div className="skills">
+      <div className="textContainer">
+        <p>
+          A computer engineering graduate adept at leveraging modern web
+          technologies to craft exceptional user experiences.
+        </p>
+        <hr />
+      </div>
+      <div className="titleContainer">
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <b>Unique</b> ideas
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <b>For Your</b> Business
+          </h1>
+          <button>What we do</button>
+        </div>
+      </div>
+      <div className="listContainer">
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem
+            consectetur dolor earum nostrum quaerat vero repellat impedit, atque
+            nesciunt placeat, est recusandae consequatur voluptate laboriosam,
+            facilis obcaecati voluptates rem fugiat!
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem
+            consectetur dolor earum nostrum quaerat vero repellat impedit, atque
+            nesciunt placeat, est recusandae consequatur voluptate laboriosam,
+            facilis obcaecati voluptates rem fugiat!
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem
+            consectetur dolor earum nostrum quaerat vero repellat impedit, atque
+            nesciunt placeat, est recusandae consequatur voluptate laboriosam,
+            facilis obcaecati voluptates rem fugiat!
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem
+            consectetur dolor earum nostrum quaerat vero repellat impedit, atque
+            nesciunt placeat, est recusandae consequatur voluptate laboriosam,
+            facilis obcaecati voluptates rem fugiat!
+          </p>
+          <button>Go</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
