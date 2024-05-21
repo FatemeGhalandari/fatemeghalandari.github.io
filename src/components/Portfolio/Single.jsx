4504540780f8f86aa348274f91c2e24b94533068
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-21 12:55:00 -0400

    feat: Add Single component for portfolio item display

---
+++ src/components/Portfolio/Single.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import "./Portfolio.scss";
+
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-500, 500]);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imgContainer" ref={ref}>
+            <img src={item.img} alt="{item.img}" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Single;
