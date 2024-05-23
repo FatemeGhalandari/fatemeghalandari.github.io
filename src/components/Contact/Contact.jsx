f638d2c5e110227f0decf2d0fa6962c6ab7a170f
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-23 13:42:29 -0400

    feat: Fix console.log statement and update success handling in Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
       })
       .then(
         (success) => {
-              setSuccess(true);
-              console.log(success)
+          setSuccess(true);
+          console.log(success);
         },
         (error) => {
           setError(true);
