a59fcde8bda3e853faba3ee12b4b44c2a7f37b50
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-24 17:20:04 -0400

    style: Convert all sass styles to tailwind

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/prop-types */
+import { useRef } from "react";
+import { motion, useScroll, useSpring } from "framer-motion";
+import Single from "./Single";
+
+const items = [
+  {
+    id: 1,
+    title: "Web Design",
+    img: "https://images.pexels.com/photos/574077/pexels-photo-574077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    title: "Web Design2",
+    img: "https://images.pexels.com/photos/574077/pexels-photo-574077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    title: "Web Design3",
+    img: "https://images.pexels.com/photos/574077/pexels-photo-574077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 4,
+    title: "Web Design4",
+    img: "https://images.pexels.com/photos/574077/pexels-photo-574077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
+const Portfolio = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, { stiffness: 100, damping: 30 });
+
+  return (
+    <div className="relative" ref={ref}>
+      <div className="sticky top-0 left-0 pt-12 text-6xl text-center text-[#9be76e]">
+        <h1>Featured works</h1>
+        <motion.div className="bg-white h-2" style={{ scaleX }}></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Portfolio;
