6b6235c6ff749c042ad0a0fd2bc19a9e1a107dc9
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-24 16:57:57 -0400

    style: Add background images for mountains, planets, and stars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        mountains: "url('/mountains.png')",
+        planets: "url('/planets.png')",
+        stars: "url('/stars.png')",
+      },
+    },
   },
   plugins: [],
 };
