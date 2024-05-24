e204633d5595d5aa0b16bf73cdd72697233a2783
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-24 14:38:05 -0400

    style: Convert all sass styles to tailwind

--- src/components/Sidebar/ToggleButton.jsx
+++ src/components/Sidebar/ToggleButton.jsx
@@ -4,7 +4,10 @@ import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 // eslint-disable-next-line react/prop-types
 const ToggleButton = ({ setOpen, open }) => {
   return (
-    <button onClick={() => setOpen((prev) => !prev)}>
+    <button
+      className="fixed z-[1] top-6 left-6 w-[50px] h-[50px] bg-transparent border-r-[50%] border-none cursor-pointer"
+      onClick={() => setOpen((prev) => !prev)}
+    >
       {open ? (
         <FontAwesomeIcon icon={faX} />
       ) : (
