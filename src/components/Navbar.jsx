da91a9f438b2f8ff038dc8980b2f0f03c927b319
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-24 14:38:37 -0400

    style: Convert all sass styles to tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+import {
+  faLinkedin,
+  faXTwitter,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Sidebar from "./Sidebar/Sidebar";
+
+const Navbar = () => {
+  return (
+    <div className="h-[100px] bg-[#1a1a2e]">
+      <Sidebar />
+      <div className="h-[100%] max-w-[1366px] flex m-auto items-center justify-between pl-5">
+        <a href="/">
+          <motion.span
+            className="font-bold"
+            initial={{ opacity: 0.5, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            Fateme Ghalandari
+          </motion.span>
+        </a>
+        <ul className="flex gap-5">
+          <a
+            href="https://www.linkedin.com/in/fghalandarii/"
+            className="w-4 h-4"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="https://github.com/FatemeGhalandari" className="w-4 h-4">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="https://twitter.com/fghalandarii" className="w-4 h-4">
+            <FontAwesomeIcon icon={faXTwitter} />
+          </a>
+          <a href="mailto:fghalandarii@gmail.com" className="w-4 h-4">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
