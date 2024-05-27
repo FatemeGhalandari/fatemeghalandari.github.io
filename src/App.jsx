e96f3732602849a4d262687e805eeda0d70d0192
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-27 15:28:51 -0400

    feat: Add handlescroll function

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,35 @@ import Navbar from "./components/Navbar";
 import Parallax from "./components/Parallax";
 import Portfolio from "./components/Portfolio";
 import Skills from "./components/Skills";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    let sections = document.querySelectorAll("section");
+    let currentSection = 0;
+    let isScrolling;
+
+    const handleScroll = (e) => {
+      clearTimeout(isScrolling);
+
+      isScrolling = setTimeout(() => {
+        if (e.deltaY > 0) {
+          // Scrolling down
+          currentSection = Math.min(sections.length - 1, currentSection + 1);
+        } else {
+          // Scrolling up
+          currentSection = Math.max(0, currentSection - 1);
+        }
+        sections[currentSection].scrollIntoView({ behavior: "smooth" });
+      },100);
+    };
+    window.addEventListener("wheel", handleScroll);
+
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, []);
+
   return (
     <div>
       <section id="Home">
