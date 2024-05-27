9700e27db223c8e2d901bc585e7d492a6ac46f14
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-27 15:37:14 -0400

    style: Update css and make responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import {
   faLinkedin,
   faXTwitter,
@@ -12,17 +12,17 @@ const Navbar = () => {
   return (
     <div className="h-[100px] bg-[#1a1a2e]">
       <Sidebar />
-      <div className="h-[100%] max-w-[1366px] flex m-auto items-center justify-between pl-5">
-        <a href="/">
+      <div className="h-[100%] max-w-[1366px] flex m-auto items-center  justify-end sm:pl-5 p-5">
+        {/* <a href="/">
           <motion.span
-            className="font-bold"
+            className="font-bold sm:block hidden font-mono"
             initial={{ opacity: 0.5, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
           >
             Fateme Ghalandari
           </motion.span>
-        </a>
+        </a> */}
         <ul className="flex gap-5">
           <a
             href="https://www.linkedin.com/in/fghalandarii/"
