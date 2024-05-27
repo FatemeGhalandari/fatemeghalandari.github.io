3cb58e4e38e7daaee6077f14708245a9824d1cee
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-27 15:29:55 -0400

    style: Update css

--- src/components/Sidebar/ToggleButton.jsx
+++ src/components/Sidebar/ToggleButton.jsx
@@ -5,7 +5,7 @@ import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 const ToggleButton = ({ setOpen, open }) => {
   return (
     <button
-      className="fixed z-[1] top-6 left-6 w-[50px] h-[50px] bg-transparent border-r-[50%] border-none cursor-pointer"
+      className="fixed z-[1] top-6 left-6 w-[50px] h-[50px] bg-transparent border-r-4 border-none cursor-pointer"
       onClick={() => setOpen((prev) => !prev)}
     >
       {open ? (
