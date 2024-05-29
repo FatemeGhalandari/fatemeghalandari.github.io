3bd49de6c9c837d12045488a4fe31fe55849c7b0
FatemeGhalandari <fghalandarii@gmail.com>
2024-05-29 12:08:33 -0400

    style: Refactor Sidebar ToggleButton component to use event.stopPropagation()

--- src/components/Sidebar/ToggleButton.jsx
+++ src/components/Sidebar/ToggleButton.jsx
@@ -3,10 +3,15 @@ import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 
 // eslint-disable-next-line react/prop-types
 const ToggleButton = ({ setOpen, open }) => {
+  const toggleSidebar = (event) => {
+    event.stopPropagation();
+    console.log("toggleSidebar: current open state", open);
+    setOpen(!open);
+  };
   return (
     <button
-      className="fixed z-[1] top-6 left-6 w-[50px] h-[50px] bg-transparent border-r-4 border-none cursor-pointer"
-      onClick={() => setOpen((prev) => !prev)}
+      className="fixed z-[20] top-6 left-6 w-[50px] h-[50px] bg-transparent border-r-4 border-none cursor-pointer"
+      onClick={toggleSidebar}
     >
       {open ? (
         <FontAwesomeIcon icon={faX} />
