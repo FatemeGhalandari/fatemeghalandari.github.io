a72755d080cd07e49f3d329d3db814c2745bddf8
FatemeGhalandari <fghalandarii@gmail.com>
2024-06-04 18:13:54 -0400

    Refactor Portfolio component description for improved clarity and conciseness

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,7 +8,7 @@ const items = [
     id: 1,
     title: "Beautyness",
     img: "Beautyness.png",
-    desc: "A modern and responsive website dedicated to beauty and body care built using ReactJS, Tailwind CSS, and Vite. The website offers a curated experience for users, delivering expertly crafted content on skincare routines and makeup tips. Rooted in a philosophy encapsulated by the slogan \"Beauty is power, a smile is its sword.\"",
+    desc: 'A modern and responsive website built using ReactJS and Tailwind CSS delivering expertly crafted content on skincare routines and makeup tips. Rooted in a philosophy encapsulated by the slogan "Beauty is power, a smile is its sword."',
     demo: "https://beautyness-bodycare.vercel.app/",
     git: "https://github.com/FatemeGhalandari/Beautyness",
   },
